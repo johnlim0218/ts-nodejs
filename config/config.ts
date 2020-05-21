2ba7faec4d2f5b7e5610c29a0e8aee7238640fe5
johnlim0218 <gwanwoo.john@gmail.com>
2020-05-21 16:05:20 +0900

    Ready to build up with sequelize

---
+++ config/config.ts
@@ -0,0 +1,45 @@
+// sequelize-cli가 ts를 인식하지 못한다.
+// js로 실행후 ts로 변경
+// sequelize db:create
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+type Config = {
+  username: string,
+  password: string,
+  database: string,
+  host: string,
+  [key: string]: string,
+}
+
+interface IConfig {
+  development: Config;
+  test: Config;
+  production: Config;
+}
+
+const config: IConfig = {
+  "development": {
+    "username": "root",
+    "password": process.env.DB_PASSWORD!,
+    "database": "johnsdb",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+  },
+  "test": {
+    "username": "root",
+    "password": process.env.DB_PASSWORD!,
+    "database": "johnsdb",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+  },
+  "production": {
+    "username": "root",
+    "password": process.env.DB_PASSWORD!,
+    "database": "johnsdb",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+  }
+}
+
+export default config;
