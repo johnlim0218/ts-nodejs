3a4b406e92ec17e962ca2c22664a4501d00552a4
johnlim0218 <gwanwoo.john@gmail.com>
2020-05-21 15:43:04 +0900

    Ready to build up

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-veryFirst
+"use strict";
+exports.__esModule = true;
+var express = require("express"); // module에 default가 없는 경우 '* as' 로 import
+var prod = process.env.NODE_ENV === 'production';
+var app = express();
+app.set('port', prod ? process.env.PORT : 3065);
+app.get('/', function (req, res, next) {
+    return res.send('RUNNING!!');
+});
+app.listen(app.get('port'), function () {
+    console.log("server is running");
+});
---
+++ index.ts
@@ -0,0 +1,58 @@
+import * as express from 'express'; // module에 default가 없는 경우 '* as' 로 import
+import * as morgan from 'morgan';
+import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
+import * as expressSession from 'express-session';
+import * as dotenv from 'dotenv';
+import * as passport from 'passport';
+import * as hpp from 'hpp';
+import * as helmet from 'helmet';
+import { Request, Response, NextFunction, Application } from 'express';
+
+dotenv.config();
+
+const prod = process.env.NODE_ENV === 'production';
+const app: Application = express();
+app.set('port', prod ? process.env.PORT: 3065);
+
+if(prod) {
+    app.use(hpp());
+    app.use(helmet());
+    app.use(morgan('combine'));
+    app.use(cors ({
+        origin: '',
+        credentials: true,
+    }));
+} else {
+    app.use(morgan('dev'));
+    app.use(cors({
+        origin: true,
+        credentials: true,
+    }))
+}
+
+app.use('/', express.static('uploads'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET!,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        domain: prod ? '' : undefined,
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    return res.send('RUNNING!!');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`server is running`);
+})
